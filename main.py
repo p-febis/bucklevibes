9793ab447b82394f5afe6780d3b960c703e55e58
p-febis <preston.febis@yahoo.com>
2025-08-22 19:13:10 -0300

    Make it work, then make it pretty

--- main.py
+++ main.py
@@ -3,11 +3,18 @@ import asyncio
 from typing import TypedDict, cast
 from ffmpeg.asyncio import FFmpeg
 
+
 class ConfigV1(TypedDict):
     id: str
     name: str
     sound: str
     defines: dict[str, list[int] | None]
+
+
+def file_path_from_key_code(keycode: str):
+    hex_keycode = "%02x" % int(keycode)
+    file_name = f"{hex_keycode}-1.wav"
+    return f"wav/{file_name}"
 
 
 async def main():
@@ -16,25 +23,30 @@ async def main():
 
     print(f"Parsing {config['name']}...")
 
-    key_codes = config['defines'].keys()
+    key_codes = config["defines"].keys()
 
     awaitables = []
 
-    for key_code in key_codes:
-        hex_keycode = "%02x" % int(key_code)
-        file_name = f"{hex_keycode}-1.wav"
-        file_path = f"wav/{file_name}"
+    for keycode in key_codes:
+        file_path = file_path_from_key_code(keycode)
 
-        array = config["defines"][key_code]
+        array = config["defines"][keycode]
         if array is None:
             continue
 
-
         start_time, duration = int(array[0]) / 1000, int(array[1]) / 1000
-        
-        ffmpeg = FFmpeg().option("y").input(config["sound"], ss=start_time, t=duration).output(file_path)
+
+        ffmpeg = (
+            FFmpeg()
+            .option("y")
+            .input(config["sound"], ss=start_time, t=duration)
+            .output(file_path)
+        )
+
         awaitables.append(ffmpeg.execute())
 
     await asyncio.gather(*awaitables)
+    print("Finished, Enjoy!")
+
 
 asyncio.run(main())
